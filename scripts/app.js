4957703aa500d9d8c6f57d9b71738b9226c003bd
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-12-23 17:08:26 +0900

    p28 HsvPalette

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+goog.privide('chapter2.App');
+goog.require('goog.ui.HsvPalette');
+
+/** @constructor */
+chapter2.App = function() {
+  // 色選択ウィジェットを作成
+  this.palette = new goog.ui.HsvPalette();
+  this.palette.render(document.getElementById('palette'));
+}
+
+// chapter2.App のインスタンスを作成
+new chapter2.App();
