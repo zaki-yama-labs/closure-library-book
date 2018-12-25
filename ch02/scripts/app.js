6b105b81da2e76ead4f656a76fd36192e0285e63
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-12-26 00:17:26 +0900

    [p32] 2.2.7 「背景色に設定」ボタンを追加する

--- ch02/scripts/app.js
+++ ch02/scripts/app.js
@@ -6,6 +6,12 @@ chapter2.App = function() {
   // 色選択ウィジェットを作成
   this.palette = new goog.ui.HsvPalette();
   this.palette.render(document.getElementById('palette'));
+
+  document.getElementById('bgcolor-btn').onclick = goog.bind(this.setBGColor, this);
+}
+
+chapter2.App.prototype.setBGColor = function() {
+  document.body.style.backgroundColor = this.palette.getColor();
 }
 
 // chapter2.App のインスタンスを作成
