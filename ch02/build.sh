279c413aee6b8e82d9b464802f04f6ce626ed035
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-12-26 00:29:24 +0900

    [p36] 2.3 closurebuilder.pyによるスクリプトの統合

--- ch02/build.sh
+++ ch02/build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-python closure-library/closure/bin/build/depswriter.py \
-  --root_with_prefix="scripts ../../../scripts" \
-  --output_file=deps.js
+
+# # depswriter.py
+# python closure-library/closure/bin/build/depswriter.py \
+#   --root_with_prefix="scripts ../../../scripts" \
+#   --output_file=deps.js
+
+# closurebuilder.py
+python closure-library/closure/bin/build/closurebuilder.py \
+  -n chapter2.App -o script --output_file=script.js \
+  --root=closure-library --root=scripts
