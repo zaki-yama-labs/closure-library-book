bfaffc4aa42719d0d89d4dc188d7d479deb54b22
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-01-04 23:30:51 +0900

    [p130] 5.4.1 ツールバーの追加

--- ch05/scripts/leftpane.js
+++ ch05/scripts/leftpane.js
@@ -2,6 +2,11 @@ goog.provide('tinyword.LeftPane');
 
 goog.require('goog.dom.classes');
 goog.require('goog.ui.Component');
+goog.require('goog.ui.Toolbar');
+goog.require('goog.ui.ToolbarMenuButton');
+goog.require('goog.ui.Menu');
+goog.require('goog.ui.MenuItem');
+goog.require('goog.array');
 
 tinyword.LeftPane = class extends goog.ui.Component {
 
@@ -10,6 +15,16 @@ tinyword.LeftPane = class extends goog.ui.Component {
    */
   constructor(opt_domHelper) {
     super(opt_domHelper);
+
+    /**
+     * @private
+     * @type {goog.ui.Toolbar}
+     */
+    this.toolbar_ = new goog.ui.Toolbar(
+      goog.ui.ToolbarRenderer.getInstance(),
+      goog.ui.Container.Orientation.HORIZONTAL,
+      opt_domHelper);
+    this.addChild(this.toolbar_);
   }
 
   /** @override */
@@ -19,13 +34,25 @@ tinyword.LeftPane = class extends goog.ui.Component {
 
   /** @override */
   canDecorate(element) {
-    return true;
+    const dom = this.getDomHelper();
+    const toolbarEl = dom.getElementsByTagNameAndClass(
+      'div', goog.ui.ToolbarRenderer.CSS_CLASS, element)[0];
+    return (toolbarEl && this.toolbar_.canDecorate(toolbarEl));
   }
 
   /** @override */
   decorateInternal(element) {
     super.decorateInternal(element);
     goog.dom.classes.add(element, tinyword.LeftPane.CLASS_NAME_);
+
+    const dom = this.getDomHelper();
+    let toolbarEl = dom.getElementsByTagNameAndClass(
+      'div', goog.ui.ToolbarRenderer.CSS_CLASS, element)[0];
+    if (!toolbarEl) {
+      toolbarEl = dom.createDom('div', goog.ui.ToolbarRenderer.CSS_CLASS);
+      dom.appendChild(element, toolbarEl);
+    }
+    this.toolbar_.decorate(toolbarEl);
   }
 
   /** @override */
