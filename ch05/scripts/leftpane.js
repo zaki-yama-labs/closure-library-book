825ffe2ea07f10be8f8239900880858419dfec8b
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-01-04 22:32:56 +0900

    [p125] 5.3.1 左ペインを実装する

---
+++ ch05/scripts/leftpane.js
@@ -0,0 +1,43 @@
+goog.provide('tinyword.LeftPane');
+
+goog.require('goog.dom.classes');
+goog.require('goog.ui.Component');
+
+tinyword.LeftPane = class extends goog.ui.Component {
+
+  constructor(opt_domHelper) {
+    super(opt_domHelper);
+  }
+
+  /** @override */
+  disposeInternal() {
+    super.disposeInternal();
+  }
+
+  /** @override */
+  canDecorate(element) {
+    return true;
+  }
+
+  /** @override */
+  decorateInternal(element) {
+    super.decorateInternal(element);
+    goog.dom.classes.add(element, tinyword.LeftPane.CLASS_NAME_);
+  }
+
+  /** @override */
+  createDom() {
+    this.decorateInternal(this.getDomHelper().createDom('div'));
+  }
+
+  /** @override */
+  enterDocument() {
+    super.enterDocument();
+  }
+
+  /** @override */
+  exitDocument(){
+    super.exitDocument();
+  }
+}
+tinyword.LeftPane.CLASS_NAME_ = goog.getCssName('leftpane');
