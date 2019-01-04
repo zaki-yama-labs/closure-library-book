c7758ac4d95c7ae66ebc5576e9b7f19c1809da49
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-01-04 22:41:40 +0900

    [p126] 5.3.2 右ペインを実装する

---
+++ ch05/scripts/rightpane.js
@@ -0,0 +1,19 @@
+goog.provide('tinyword.RightPane');
+
+goog.require('goog.ui.Component');
+
+tinyword.RightPane = class extends goog.ui.Component {
+
+  constructor(opt_domHelper) {
+    super(opt_domHelper);
+  }
+
+  /** @override */
+  createDom() {
+    const dom = this.getDomHelper();
+    this.setElementInternal(
+      dom.createDom('div', tinyword.RightPane.CLASS_NAME_)
+    );
+  }
+}
+tinyword.RightPane.CLASS_NAME_ = goog.getCssName('rightpane');
